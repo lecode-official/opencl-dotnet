9d6f02806244a71fe04806b222a2d5c7c875b254
David Neumann <david.neumann@lecode.de>
2016-12-20 17:52:41 +0100

    Finished the native interop for the profiling API.

--- OpenCl.DotNetCore.Interop/CommandQueues/CommandQueueProperty.cs
+++ OpenCl.DotNetCore.Interop/CommandQueues/CommandQueueProperty.cs
@@ -16,21 +16,21 @@ namespace OpenCl.DotNetCore.Interop.CommandQueues
         /// <summary>
         /// Determines whether the commands queued in the command-queue are executed in-order or out-of-order. If set, the commands in the command-queue are executed out-of-order. Otherwise, commands are executed in-order.
         /// </summary>
-        QueueOutOfOrderExecutionModeEnable = 1 << 0,
+        OutOfOrderExecutionModeEnable = 1 << 0,
 
         /// <summary>
         /// Enables or disables profiling of commands in the command-queue. If set, the profiling of commands is enabled. Otherwise profiling of commands is disabled.
         /// </summary>
-        QueueProfilingEnable = 1 << 1,
+        ProfilingEnable = 1 << 1,
 
         /// <summary>
         /// 
         /// </summary>
-        QueueOnDevice = 1 << 2,
+        OnDevice = 1 << 2,
 
         /// <summary>
         /// 
         /// </summary>
-        QueueOnDeviceDefault = 1 << 3
+        OnDeviceDefault = 1 << 3
     }
 }
