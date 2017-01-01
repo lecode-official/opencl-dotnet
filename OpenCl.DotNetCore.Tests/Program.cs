3126c0fcd3f5417d4701e4048c25be1306b15f47
David Neumann <david.neumann@live.com>
2017-01-01 18:31:14 +0100

    Now extensive information about the platforms and the available devices is displayed.

--- OpenCl.DotNetCore.Tests/Program.cs
+++ OpenCl.DotNetCore.Tests/Program.cs
@@ -2,7 +2,9 @@
 #region Using Directives
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using ConsoleTables;
 
 #endregion
 
@@ -21,15 +23,29 @@ namespace OpenCl.DotNetCore.Tests
         /// <param name="args">The command line arguments that have been passed to the program.</param>
         public static void Main(string[] args)
         {
-            // Gets the first available platform and selects the first device offered by the platform
-            Platform platform = Platform.GetPlatforms().FirstOrDefault();
-            Device device = platform.GetDevices(DeviceType.All).FirstOrDefault();
+            // Gets all available platforms and their corresponding devices, and prints them out in a table
+            IEnumerable<Platform> platforms = Platform.GetPlatforms();
+            ConsoleTable consoleTable = new ConsoleTable("Platform", "Vendor", "Device", "Bits", "Memory");
+            foreach (Platform platform in platforms)
+            {
+                foreach (Device device in platform.GetDevices(DeviceType.All))
+                    consoleTable.AddRow(
+                        platform.Name,
+                        platform.Vendor,
+                        device.Name,
+                        $"{device.AddressBits} Bit",
+                        $"{Math.Round(device.GlobalMemorySize / 1024.0f / 1024.0f / 1024.0f, 2)} GiB");
+            }
+            consoleTable.Write(Format.Alternative);
+            Console.WriteLine();
 
-            // Prints out information about the selected device
-            Console.WriteLine($"Using {device.Name} ({device.Vendor}, {device.AddressBits} Bit, {device.GlobalMemorySize / 1024.0f / 1024.0f / 1024.0f} GiB Global Memory)");
+            // Gets the first available platform and selects the first device offered by the platform and prints out the chosen device
+            Device chosenDevice = platforms.FirstOrDefault().GetDevices(DeviceType.All).FirstOrDefault();
+            Console.WriteLine($"Using {chosenDevice.Name} ({chosenDevice.Vendor})");
+            Console.WriteLine();
 
             // Creats a new context for the selected device
-            using (Context context = Context.CreateContext(device))
+            using (Context context = Context.CreateContext(chosenDevice))
             {
                 // Creates the kernel code, which multiplies a matrix with a vector
                 string code = @"
@@ -65,7 +81,7 @@ namespace OpenCl.DotNetCore.Tests
                             kernel.SetKernelArgument(2, result);
                             
                             // Creates a command queue, executes the kernel, and retrieves the result
-                            using (CommandQueue commandQueue = CommandQueue.CreateCommandQueue(context, device))
+                            using (CommandQueue commandQueue = CommandQueue.CreateCommandQueue(context, chosenDevice))
                             {
                                 commandQueue.EnqueueNDRangeKernel(kernel, 1, 4);
                                 float[] resultArray = commandQueue.EnqueueReadBuffer<float>(result, 4);
