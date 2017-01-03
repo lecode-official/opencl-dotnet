11dddb289db77f56573aba11b115185f63256363
David Neumann <david.neumann@lecode.de>
2017-01-03 16:57:14 +0100

    Finished the memory native API.

--- OpenCl.DotNetCore.Interop/Memory/BufferCreateType.cs
+++ OpenCl.DotNetCore.Interop/Memory/BufferCreateType.cs
@@ -8,7 +8,7 @@ namespace OpenCl.DotNetCore.Interop.Memory
     {
 
         /// <summary>
-        /// 
+        /// Creates a buffer object that represents a specific region in the buffer.
         /// </summary>
         Region = 0x1220
     }
