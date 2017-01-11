b4a89edeb745ab428d5966d96c22b3379fbcec0a
David Neumann <david.neumann@lecode.de>
2017-01-11 13:16:38 +0100

    Renamed Buffer to MemoryBuffer to resolve the conflict to System.Buffer.

--- OpenCl.DotNetCore/Memory/MemoryBuffer.cs
+++ OpenCl.DotNetCore/Memory/MemoryBuffer.cs
@@ -8,17 +8,17 @@ using System;
 namespace OpenCl.DotNetCore.Memory
 {
     /// <summary>
-    /// Represents an OpenCL buffer.
+    /// Represents an OpenCL memory buffer.
     /// </summary>
-    public class Buffer : MemoryObject
+    public class MemoryBuffer : MemoryObject
     {
         #region Constructors
 
         /// <summary>
         /// Initializes a new <see cref="Buffer"/> instance.
         /// </summary>
-        /// <param name="handle">The handle to the OpenCL buffer.</param>
-        public Buffer(IntPtr handle)
+        /// <param name="handle">The handle to the OpenCL memory buffer.</param>
+        public MemoryBuffer(IntPtr handle)
             : base(handle)
         {
         }
